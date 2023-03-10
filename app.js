4fb3e1b4014c85f158d9eb1b0649e814f9679876
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-10 10:53:30 -0600

    finished exercises

--- app.js
+++ app.js
@@ -5,15 +5,45 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
-
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 // YOUR CODE HERE
-
+// What is the value of: num + str? "1111"
+// What is the value of: num + str2? "11eleven"
+// What is the value of: num + isPresent? 12
+// What is the value of: firstName + num? "Frodo11"
+// What is the value of: isPresent + str? "true11"
+// What is the value of: firstName + lastName? "FrodoBaggins"
 // Exercise 3
+
 console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
 
 // YOUR CODE HERE
+// What is the value of: val == str3? true
+// What is the value of: val === str3? false
+// What is the value of: !isPresent2? true
+// What is the value of: (“eleven” == str4 && val >= str3)? false
+// What is the value of: (!isPresent2 || isPresent2)? true
+// What is the value of: 0 == false? true
+// What is the value of: 0 === false? false
+// What is the value of: 0 != false? false
+// What is the value of 0 !== false? true
